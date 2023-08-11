0f8d53604f237d0203a169d390fa0ab5f8f1cd68
7860alam <137377298+7860alam@users.noreply.github.com>
2023-08-11 20:31:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "25479482"))
-API_HASH = os.environ.get("API_HASH", "6ab604ff91a73fb91cc6526818e28ab1")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6205717241:AAENuooA8cw-zcZgPazxRqawsoVdJeDUo5Y")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5736579519")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "25833572"))
+API_HASH = os.environ.get("API_HASH", "ea383f30d64b8d9828eeca5287932d97)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5870227633:AAGV7CLZxtYSJCLPmImhAWegH-hqBpXgevc")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5010869192")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Moneycase")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "5736579519")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "ts78692123")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ts78692123:48oJg9AnKCSdziV5@cluster0.kuaoog9.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5010869192")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append("5736579519")
+ADMINS.append("5010869192")
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001774099134")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MoneyCaseOfficial") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001927957063")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Ziplinker") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
