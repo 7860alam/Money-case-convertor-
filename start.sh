bab64b36db926008ff9f307f2bedf6b35cd6be8e
7860alam <137377298+7860alam@users.noreply.github.com>
2023-08-11 22:30:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/7860alam/Money-case-convertor-.git /Money-case-convertor-
-cd /Shortner-Converter-Bot-V2
+cd /Money-case-convertor-
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
